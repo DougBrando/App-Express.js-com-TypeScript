2826be2e36a159ef2c21033f26f65c22f771b48e
Douglas Graça <159181176+DougBrando@users.noreply.github.com>
2025-01-23 10:28:55 -0300

    Update clients.ts
    Adicionadas rotas para o controlador de clientes:
    - Rota para a página inicial ("/") associada à função 'index'.
    - Rota para a página de contato ("/contact") associada à função 'contact'.
    - Rota para a página "Sobre" ("/about") associada à função 'about'.

--- appExpressJs/Router/clients.ts
+++ appExpressJs/Router/clients.ts
@@ -1,11 +1,16 @@
-// Router/clients.ts
-import { Router } from "express";
-import * as ClientsController from "../Controller/clientsControllers"; // Importação correta
+import { Router } from "express"; // Importa o módulo Router do Express para criar um roteador.
+import * as ClientsController from "../Controller/clientsControllers"; // Importa todas as funções do controlador de clientes.
 
-const router = Router();
+const router = Router(); // Cria uma nova instância do roteador.
 
+ // Define a rota para a página inicial ("/") e associa à função 'index' do ClientsController.
 router.get("/", ClientsController.index);
+
+// Define a rota para a página de contato ("/contact") e associa à função 'contact' do ClientsController.
 router.get('/contact', ClientsController.contact); // Corrigido para usar a função contact
+
+// Define a rota para a página "Sobre" ("/about") e associa à função 'about' do ClientsController.
 router.get('/about', ClientsController.about);
 
+// Exporta o roteador para que possa ser utilizado em outros arquivos (como app.ts).
 export default router;
